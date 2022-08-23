a814d3b93eaca85a6eddf7d39b3d937b4053afcd
pablo-marrero <pablomarrero241096@gmail.com>
2022-08-23 01:33:43 -0300

    yes now!

--- api/index.js
+++ api/index.js
@@ -28,7 +28,7 @@ const { default: axios } = require('axios');
 
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
